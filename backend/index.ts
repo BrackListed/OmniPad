18ac7bade9ab3e301ab2f091becc97de94b26e84
BrackListed <cameta8900@gmail.com>
2026-08-21 20:41:37 +0800

    Hardcoded frontend origin in cors

--- backend/index.ts
+++ backend/index.ts
@@ -18,10 +18,10 @@ const pool = new Pool({connectionString: process.env.DATABASE_URL})
 const db = drizzle(process.env.DATABASE_URL!)
 
 // Dynamically handles your local dev or your deployed Render frontend URL
-const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173"
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173" 
 
 app.use(cors({
-  origin: [allowedOrigin], 
+  origin: [allowedOrigin, 'https://omnipad-sepia.vercel.app'], 
   credentials: true
 }))
 
